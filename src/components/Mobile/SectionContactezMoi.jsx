79dff772076b71b9c4736ca458d0a952334015f2
POCHIC Olivier <o.pochic@gmail.com>
2024-06-12 20:42:10 +0200

    Avec Davy : externalisation avec 2 hooks (envoi de mail)

--- src/components/Mobile/SectionContactezMoi.jsx
+++ src/components/Mobile/SectionContactezMoi.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import emailjs from "@emailjs/browser";
 import ReCAPTCHA from "react-google-recaptcha";
 import { Scotch } from "../Scotch";
+import { useEmailForm } from "../../hooks/useEmailForm";
+import { useEmailJS } from "../../hooks/useSendEmail";
 
 export const SectionContactezMoi = () => {
 	let canSendMail = false;
@@ -11,23 +13,18 @@ export const SectionContactezMoi = () => {
 		canSendMail = value;
 	}
 
-	const [emailData, setEmailData] = useState({
-		user_name: "",
-		user_email: "",
-		user_phone: "",
-		message_subject: "",
-		message_content: "",
-	});
+	const { formData, handleChange, reset } = useEmailForm();
 
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setEmailData({
-			...emailData,
-			[name]: value,
-		});
-	};
+	const { send, status } = useEmailJS();
 
-	useEffect(() => {}, []);
+	// useEffect(() => {
+	// 	if (status === "success") {
+	// 		alert("Votre email à bien été envoyé !");
+	// 		reset();
+	// 	} else if (status === "failure") {
+	// 		alert("Failed to send email.");
+	// 	}
+	// }, [status]);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -35,43 +32,31 @@ export const SectionContactezMoi = () => {
 			console.log("Veuillez valider votre Captcha");
 			return false;
 		}
-		emailjs.send(process.env.REACT_APP_EMAILJS_SERVICE_ID, process.env.REACT_APP_EMAILJS_TEMPLATE_ID, emailData, process.env.REACT_APP_EMAILJS_USER_ID).then(
-			(response) => {
-				console.log("SUCCESS!", response.status, response.text);
-				alert("Votre email à bien été envoyé !");
-				setEmailData({
-					user_name: "",
-					user_email: "",
-					user_phone: "",
-					message_subject: "",
-					message_content: "",
-				});
-			},
-			(error) => {
-				console.log("FAILED...", error);
-				alert("Failed to send email.");
-			}
-		);
+
+		send(formData);
 	};
 
 	return (
 		<section id="CON" className="contactez-moi">
 			<form onSubmit={handleSubmit}>
+				{status === "success" && <p>Envoi OK x</p>}
+				{status === "failure" && <p>Envoi KO x</p>}
 				<h2>CONTACTEZ-MOI</h2>
 				<div className="scotch-contactez-moi-1">
 					<Scotch on="mobile" text={"Vos informations"} angle={-3} size={"long"} style={{ marginLeft: "4rem" }} />
 				</div>
-				<input type="text" name="user_name" placeholder="Votre nom" value={emailData.user_name} onChange={handleChange} required />
-				<input type="text" name="user_email" placeholder="Votre email" value={emailData.user_email} onChange={handleChange} required />
-				<input type="text" name="user_phone" placeholder="Votre numéro (optionnel)" value={emailData.user_phone} onChange={handleChange} />
-				<input type="text" name="message_subject" placeholder="Votre sujet" value={emailData.message_subject} onChange={handleChange} required />
+				<input type="text" name="userName" placeholder="Votre nom" value={formData.userName} onChange={handleChange} required />
+				<input type="text" name="userEmail" placeholder="Votre email" value={formData.userEmail} onChange={handleChange} required />
+				<input type="text" name="userPhone" placeholder="Votre numéro (optionnel)" value={formData.userPhone} onChange={handleChange} />
+				<input type="text" name="messageSubject" placeholder="Votre sujet" value={formData.messageSubject} onChange={handleChange} required />
 
 				<div className="scotch-contactez-moi-3">
 					<Scotch on="mobile" text={"Votre message"} angle={1} size={"long"} style={{ marginLeft: "2rem" }} />
 				</div>
-				<textarea name="message_content" value={emailData.message_content} onChange={handleChange} required></textarea>
+				<textarea name="messageContent" value={formData.messageContent} onChange={handleChange} required></textarea>
 				<ReCAPTCHA sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY} onChange={onChange} />
 				<button type="submit">Envoyer le mail</button>
+				{status === "sending" && <p>envoi en cours...</p>}
 			</form>
 		</section>
 	);
---
+++ src/hooks/useEmailForm.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export const useEmailForm = () => {
+	const initialData = {
+		userName: "",
+		userEmail: "",
+		userPhone: "",
+		messageSubject: "",
+		messageContent: "",
+	};
+
+	const [formData, setFormData] = useState(initialData);
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({
+			...formData,
+			[name]: value,
+		});
+	};
+
+	const reset = () => {
+		setFormData(initialData);
+	};
+
+	return { formData, handleChange, reset };
+};
