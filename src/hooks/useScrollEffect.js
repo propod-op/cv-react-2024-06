47b0bc816a55a69b96190e08721b98394bbcac40
olivier <o.pochic@gmail.com>
2024-07-08 21:11:47 +0200

    DavnDev : hooks simplifiés et fonctionnels

--- src/hooks/useScrollEffect.js
+++ src/hooks/useScrollEffect.js
@@ -4,20 +4,22 @@ import { useScrollPosition } from './useScrollPosition';
 // Ce Hook renvoi une classe 'exposed' ou 'notExposed' en fonction de la scrollPosition
 export const useScrollEffect = (positionStart, positionEnd) => {
   const { scrollPosition } = useScrollPosition();
-  const [exposedClass, setExposedClass] = useState('');
+  // const [exposedClass, setExposedClass] = useState('');
 
   const condition =
     scrollPosition >= positionStart && scrollPosition < positionEnd;
 
-  useEffect(() => {
-    if (condition) {
-      //console.log('class exposed');
-      setExposedClass('exposed');
-    } else {
-      //console.log('class notExposed');
-      setExposedClass('notExposed');
-    }
-  }, [condition]);
+  const exposedClass = condition ? 'exposed' : 'notExposed';
+
+  // useEffect(() => {
+  //   if (condition) {
+  //     //console.log('class exposed');
+  //     setExposedClass('exposed');
+  //   } else {
+  //     //console.log('class notExposed');
+  //     setExposedClass('notExposed');
+  //   }
+  // }, [condition]);
 
   return { exposedClass };
 };
